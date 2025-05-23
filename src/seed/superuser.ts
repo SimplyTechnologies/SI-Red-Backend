78dab3aca3d357a30c3e9353adb4c12dc855a805
Kehyan Levon <mrkheyan@gmail.com>
2025-05-23 15:12:31 +0400

    testing husky 2

--- src/seed/superuser.ts
+++ src/seed/superuser.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import { User } from '../models/User.model';
 import { sequelize } from '../config/db';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
 
